ea6c01a30b6ff26ca82188f5332a1eecc1673d39
RaunaqRaj <raunaqraj2003@gmail.com>
2022-04-16 08:05:21 +0530

    comments written

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -9,20 +9,24 @@ use Auth;
 
 class authController extends Controller
 {
+    //login with google api
     public function login(){
         return view('login');
     }
 
+    //redirect to google page
     public function google_redirect(){
         return Socialite::driver('google')->redirect();
     }
 
+     //redirect to dashboard
     public function google_callback(){
         $user = Socialite::driver('google')->user();
         $this->createorupdateuser($user,'google');
         return redirect('dashboard');
     }
 
+     //Storing of data in database
     private function createorupdateuser($data,$provider){
         $user = User::where('email',$data->email)->first();
         if ($user) {

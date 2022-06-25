972795815719d3a2a0c6c0cea6f3f1102cb984da
Raunak Sharma <waytoraunak521@gmail.com>
2022-06-25 09:40:22 +0530

    first commit

--- app/Console/Commands/DBinfo.php
+++ app/Console/Commands/DBinfo.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class DBinfo extends Command
+class DBinfo extends Command 
 {
     /**
      * The name and signature of the console command.
